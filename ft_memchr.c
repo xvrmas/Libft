0efc195f9362342052dc8b8d6bda987099725dc2
Xavier Mas <xvrmasgarcia@gmail.com>
2023-05-20 14:06:25 +0200

    copy new folder

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,26 +1,33 @@
 #include <stdio.h>
 #include <string.h>
- void *ft_memchr(const void *s, int c, size_t n)
+ void	*ft_memchr(const void *s, int c, size_t n)
  {
-    char *str;
-    str = s;
-    int i;
-    while (i < n)
-    {
-        
+    size_t		i;
+    int	found;
+    
+    i = 0;
+    found = 0;
+    while (i < n && found = 0)
+    
+       if (*s[i] == c)
+       {
+	      return (*s[i]);
+	      found = 1;
+       }
+      i++; 
     }
-    printf("%s\n", str);
+    /*printf("%s\n", str);
     printf("%c\n", c);
-    printf("%d\n", n);
+    printf("%d\n", n);*/
  }
 int main()
 {
    char cadena[] = "Erase una vez...";
    char *puntero; 
  
-   /*puntero = (char *)memchr( cadena, 'e', 8 );
+   puntero = (char *)memchr( cadena, 'e', 8 );
    printf( "%s\n", cadena); 
-   printf( "%s\n", puntero ); */
+   printf( "%s\n", puntero );
    ft_memchr(cadena, 'e', 8);
  
    return 0;
