5a2c26a87ea4d72e645d167aa706260a4459ffab
Xavier Mas <xvrmasgarcia@gmail.com>
2023-05-21 20:42:04 +0200

    strdup ok

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,20 +6,47 @@
 /*   By: xavier <xamas-ga@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 19:39:53 by xavier            #+#    #+#             */
-/*   Updated: 2023/05/21 19:41:19 by xavier           ###   ########.fr       */
+/*   Updated: 2023/05/21 20:38:26 by xavier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "libft.h"
 
-char *strdup(const char *s)
+
+char *ft_strdup(const char *s)
 {
+ char len;
+        char *str;
+        int i;
+        
+        i = 0;
+        len = ft_strlen(s);
+        str = malloc(len + 1);
+	if (!str)
+		return (NULL);
+        while (i < len)
+        {
+          str[i] = s[i];
+          i++;
+        }
+        str[i] = '\0';
+        return (str);
 
 }
-int main() {
+/*int main() {
   
-  char str[] = "Hola Barcelona";
+  char str[] = "Hola 42  Barcelona";
+  //char str2[] = "Hola Barcelona";
   char *s1;
+  char *s2;
+
   s1 = strdup(str);
-  printf("%s", s1);
+  printf("strdup: %s\n", s1);
+  printf("-------------------------------\n");
+  s2 = ft_strdup(str);
+  printf("ft_strdup: %s\n", s2);
 
   return 0;
-}
+}*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: xamas-ga <xamas-ga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/14 17:15:03 by xamas-ga          #+#    #+#             */
-/*   Updated: 2023/05/21 18:45:05 by xavier           ###   ########.fr       */
+/*   Updated: 2023/05/21 20:34:02 by xavier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,5 +35,6 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
 void *ft_calloc(size_t nmemb, size_t size);
+char *ft_strdup(const char *s);
 
 #endif
