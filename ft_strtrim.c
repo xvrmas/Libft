79452f89fe2e3f126b4d99d1f5a7ecbb2106d8f1
Xavier Mas <xvrmasgarcia@gmail.com>
2023-05-29 20:05:54 +0200

    strtrim ok

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,57 +6,61 @@
 /*   By: xamas-ga <xamas-ga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 18:08:34 by xamas-ga          #+#    #+#             */
-/*   Updated: 2023/05/26 18:56:29 by xamas-ga         ###   ########.fr       */
+/*   Updated: 2023/05/29 20:03:05 by xavier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "libft.h"
 
-char    *ft_strchr(const char *str, int c)
+static int      ft_lencut(char const *s1, char c)
 {
-    int    i;
-    int    found;
+        int	i;
 
-    i = 0;
-    found = 0;
-    printf("entrada str--> %s\n", str);
-    printf("entrada c--> %c\n", c);
-    while (found == 0 && ((char *)str)[i] != '\0')
-    {
-        if (str[i] == (char)c)
-        {
-            return ((char *)&str[i]);
-            found = 1;
-        }
-        i++;
-    }
-    if ((char)c == '\0')
-        return ((char *)&str[i]);
-        printf("salida--> %s\n", str);
-    return (NULL);
-}
-char *ft_strtrim(char const *s1, char const *set)
-{
-        size_t i;
-        char *s3;
-        size_t len;
-        int found;
-
-        i  = 0;
-        found = 0;
-        len = strlen(s1);
-        s3 = ft_strchr(s1, *set);
-        printf("-->%s\n", s3);
-        return (s3);
-}
-int main()
-{
-        char s1[] = "42 Barcelona";
-        char set[] = "e";
-        char *s3;
-
-        s3 = ft_strtrim(s1, set);
-        printf("return s3-->%s\n", s3);
+        i = 0;
+        while (s1[i])
+	{
+            if (s1[i] == c)
+                return (1);
+	    i++;
+	}
         return (0);
 }
+char    *ft_strtrim(char const *s1, char const *set)
+{
+        size_t i;
+	size_t j;
+        size_t len;
+	char	*res;
+
+	i = 0;
+	j = 0;
+	len = ft_strlen(s1);
+	if (!s1)
+		return (NULL);
+	while (s1[i] && ft_lencut(set, s1[i]))
+		i++;
+	while (len > i && ft_lencut(set ,s1[len - 1]))
+		len--;
+	if (len < i)
+		len = i;
+	res = malloc((len - i) + 1 * sizeof(char));
+	if (res == NULL)
+		return (NULL);
+	while (i < len)
+		res[j++] = s1[i++];
+	res[j] = '\0';
+	return (res);
+}
+
+/*int main()
+{
+  char s1[] = "42 Barcelona";
+  char set[] = "42 ona";
+  char *s3;
+
+  s3 = ft_strtrim(s1,set);
+  printf("final s3-->  %s\n", s3);
+  return (0);
+}*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: xamas-ga <xamas-ga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/14 17:15:03 by xamas-ga          #+#    #+#             */
-/*   Updated: 2023/05/26 17:20:09 by xamas-ga         ###   ########.fr       */
+/*   Updated: 2023/05/27 13:10:33 by xavier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,4 +39,5 @@ void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char    *ft_strtrim(char const *s1, char const *set);
 #endif
