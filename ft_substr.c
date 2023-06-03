cbda37009a64ccfc0f4f837a47ef737f690de139
Xavier Mas <xvrmasgarcia@gmail.com>
2023-06-03 19:05:20 +0200

    strjoin ko

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: xamas-ga <xamas-ga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 18:39:28 by xamas-ga          #+#    #+#             */
-/*   Updated: 2023/05/23 21:03:01 by xamas-ga         ###   ########.fr       */
+/*   Updated: 2023/06/03 13:43:58 by xavier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -29,7 +29,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		len = ft_strlen(s) - start;
 	str = malloc((len + 1) * sizeof(char));
 	if (str == NULL)
-		return (ft_strdup(""));
+		return (NULL);
 	while (i < len)
 	{
 		str[i] = s[j];
