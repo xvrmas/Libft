d88e8381ac581043fd40221501f31142c9dc06c9
Xavier Mas <xvrmasgarcia@gmail.com>
2023-06-15 19:48:02 +0200

    nodos ok

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: xavier <xamas-ga@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 17:14:57 by xavier            #+#    #+#             */
-/*   Updated: 2023/06/15 19:38:07 by xavier           ###   ########.fr       */
+/*   Updated: 2023/06/15 19:47:32 by xavier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -31,15 +31,25 @@ int main()
 	t_list *node2 = ft_lstnew("cirera");
 	t_list *node3 = ft_lstnew("poma");
 	t_list *node4 = ft_lstnew("Platans");
+	t_list *node5 = ft_lstnew("cigrons");
+	t_list *node6 = ft_lstnew("22");
+	t_list *node7 = ft_lstnew("3");
+	t_list *node8 = ft_lstnew("LLenties");
 	node1->next = node2;
 	node2->next = node3;
 	node3->next = node4;
-	node4->next = NULL;
-	t_list *currentNode = node1;
-    	while (currentNode != NULL)
+	node4->next = node5;
+	node5->next = node6;
+	node6->next = node7;
+	node7->next = node8;
+	node8->next = NULL;
+	t_list *current_node = node1;
+	printf("contingut dels nodes: ");
+    	while (current_node != NULL)
     	{
-       		 printf("Contenido del nodo: %s\n", (char *)currentNode->content);
-       		 currentNode = currentNode->next;
+       		 printf("%s ", (char *)current_node->content);
+       		 current_node = current_node->next;
     	}
+	printf("\n");
 	return (0);	
 }
